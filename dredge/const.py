14d1e0d047a0ee22aa21432b26c1f90b7e63ca52
Aaron Tran <atran@physics.wisc.edu>
2025-11-11 12:49:08 -0600

    Electron mass bugfix!!

--- dredge/const.py
+++ dredge/const.py
@@ -5,7 +5,7 @@ Physical constants and unit conversions
 # Fundamental constants in Gaussian CGS units
 CLIGHT = 2.997925e10  # cm/s
 ESU = 4.80326e-10  # elementary charge in statcoulomb
-M_ELECTRON = 9.11e-27  # electron mass in grams
+M_ELECTRON = 9.11e-28  # electron mass in grams
 M_PROTON = 1.67e-24  # proton mass in grams
 
 # Unit conversions
